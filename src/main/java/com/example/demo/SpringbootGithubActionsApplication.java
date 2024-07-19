4aa824414009d78f5c4d3f69793c316529fc95f4
maheshhavnoor <mphavnoor@gmail.com>
2024-07-19 22:15:08 +0530

    modified getmapping in main file

--- src/main/java/com/example/demo/SpringbootGithubActionsApplication.java
+++ src/main/java/com/example/demo/SpringbootGithubActionsApplication.java
@@ -2,12 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class SpringbootGithubActionsApplication {
 
-	@RequestMapping("/message")
+	@GetMapping("/message")
 	public String getMessage() {
 		return "Welcome to Git hub actions";
 	}
